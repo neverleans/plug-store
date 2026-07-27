54b1ad2e70a6380ec894d647a10d103200fd6797
neverleans <lucasventavele@hotmail.com>
2026-07-27 09:33:12 -0300

    chore(docs): stop tracking Docusaurus build artifacts

--- apps/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Micro\\Documents\\Lucas Projects Git\\plug-store\\node_modules\\.pnpm\\infima@0.2.0-alpha.45\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Micro\\Documents\\Lucas Projects Git\\plug-store\\node_modules\\.pnpm\\@docusaurus+theme-classic@3_982ec91b51f34a8d2c0990277f8e3dca\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Micro\\Documents\\Lucas Projects Git\\plug-store\\node_modules\\.pnpm\\@docusaurus+theme-classic@3_982ec91b51f34a8d2c0990277f8e3dca\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Micro\\Documents\\Lucas Projects Git\\plug-store\\apps\\docs\\src\\css\\custom.css"),
-];
